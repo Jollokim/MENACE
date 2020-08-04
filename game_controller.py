9952416bad161a619c6340ec3d198c452f00aa10
Jollokim <joakimjensenj@gmail.com>
2020-08-04 16:23:23 +0200

    PEP fixes

--- game_controller.py
+++ game_controller.py
@@ -1,13 +1,14 @@
 import random
 class GameController:
-    def startGame(player1, player2):
+
+    def start_game(player1, player2):
         randint = random.randint(0,1)
         print(randint)
 
         if randint == 0:
-            player1.playMove()
+            player1.play_move()
         else:
-            player2.playMove
+            player2.play_move
 
 
 
